f08eb280507d35b6dcdc0b76711daddda87fc34c
YMaiNgoc <ymaingoc629@gmail.com>
2025-04-15 21:50:49 +0700

    commit

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../Header/Header';
+import Content from '../Content/Content';
+import Footer from '../Footer/Footer';
+function App() {
+  return (
+          <div id="container">
+            <Header/>
+            <Content/>
+           <Footer/>
+         
+          </div>
+        );
+      }
+  
+
+
+export default App;
